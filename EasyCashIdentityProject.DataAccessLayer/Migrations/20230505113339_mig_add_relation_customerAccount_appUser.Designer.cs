c080e3c27dd7295af9460e2dd715639c5f04fbbb
Enes Cicek <58270544+Cicekenes@users.noreply.github.com>
2023-05-05 15:15:18 +0300

    Dtos and validation rules added

--- EasyCashIdentityProject.DataAccessLayer/Migrations/20230505113339_mig_add_relation_customerAccount_appUser.Designer.cs
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230505113339_mig_add_relation_customerAccount_appUser.Designer.cs
@@ -4,6 +4,7 @@ using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyCashIdentityProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230505113339_mig_add_relation_customerAccount_appUser")]
+    partial class mig_add_relation_customerAccount_appUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -183,6 +185,9 @@ namespace EasyCashIdentityProject.DataAccessLayer.Migrations
 
                     b.Property<decimal>("Amount")
                         .HasColumnType("decimal(18,2)");
+
+                    b.Property<int>("MyProperty")
+                        .HasColumnType("int");
 
                     b.Property<DateTime>("ProcessDate")
                         .HasColumnType("datetime2");
